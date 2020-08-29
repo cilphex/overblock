8d5d2586d26c520005a536f00fe65cb5e96172e6
Craig Hammell <craig@coinbase.com>
2020-08-29 15:28:04 -0700

    Prod script (#8)
    Split start-lnd.sh into dev and prod files

--- services/lnd/start-lnd.dev.sh
+++ services/lnd/start-lnd.dev.sh
@@ -28,38 +28,31 @@ assert "$RPCUSER" "RPCUSER must be specified"
 assert "$RPCPASS" "RPCPASS must be specified"
 assert "$DEBUG_LEVEL" "DEBUG_LEVEL must be specified"
 
-PARAMS=""
-
-# For development purposes you may specify for the node to start up without a
-# seed phrase. This option should never be used in production.
-if [ "$NOSEEDBACKUP" = "true" ]; then
-  PARAMS="--noseedbackup"
-fi
-
 # --lnddir is an option, but it will override
 # --adminmacaroonpath and --tlscertpath.
 #
 # Optional:
-# --autopilot.active \
-PARAMS=$(echo "$PARAMS" \
-  "--alias=$ALIAS" \
-  --bitcoin.active \
-  --adminmacaroonpath=/shared/admin.macaroon \
-  --tlscertpath=/shared/tls.cert \
-  --tlskeypath=/lnd/tls.key \
-  --tlsextraip=0.0.0.0 \
-  --tlsextradomain=lnd \
-  --datadir=/lnd/data \
-  --logdir=/lnd/logs \
-  "--bitcoin.$NETWORK" \
-  "--bitcoin.node=$BACKEND" \
-  "--$BACKEND.rpccert=/rpc/rpc.cert" \
-  "--$BACKEND.rpchost=$RPCHOST" \
-  "--$BACKEND.rpcuser=$RPCUSER" \
-  "--$BACKEND.rpcpass=$RPCPASS" \
-  "--rpclisten=0.0.0.0:10009" \
+# --autopilot.active
+# --noseedbackup
+LND_PARAMS=(
+  "--alias=$ALIAS"
+  "--bitcoin.active"
+  "--adminmacaroonpath=/shared/admin.macaroon"
+  "--tlscertpath=/shared/tls.cert"
+  "--tlskeypath=/lnd/tls.key"
+  "--tlsextraip=0.0.0.0"
+  "--tlsextradomain=lnd"
+  "--datadir=/lnd/data"
+  "--logdir=/lnd/logs"
+  "--bitcoin.$NETWORK"
+  "--bitcoin.node=$BACKEND"
+  "--$BACKEND.rpccert=/rpc/rpc.cert"
+  "--$BACKEND.rpchost=$RPCHOST"
+  "--$BACKEND.rpcuser=$RPCUSER"
+  "--$BACKEND.rpcpass=$RPCPASS"
+  "--rpclisten=0.0.0.0:10009"
   "--debuglevel=$DEBUG_LEVEL"
 )
 
-echo "Starting lnd"
-exec lnd $PARAMS
+echo "Starting lnd (dev)"
+exec lnd ${LND_PARAMS[*]}
---
+++ services/lnd/start-lnd.prod.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Inspired by the lnd example:
+# https://github.com/lightningnetwork/lnd/blob/master/docker/lnd/start-lnd.sh
+
+# exit from script if error was raised.
+set -e
+
+error() {
+  echo "$1" > /dev/stderr
+  exit 0
+}
+
+assert() {
+  BLANK_STRING='""'
+  VARIABLE="$1"
+  MSG="$2"
+
+  if [[ -z "$VARIABLE" || "$VARIABLE" == "$BLANK_STRING" ]]; then
+    error "$MSG"
+  fi
+}
+
+assert "$NETWORK" "NETWORK must be specified"
+assert "$BACKEND" "BACKEND must be specified"
+assert "$RPCHOST" "RPCHOST must be specified"
+assert "$RPCUSER" "RPCUSER must be specified"
+assert "$RPCPASS" "RPCPASS must be specified"
+assert "$DEBUG_LEVEL" "DEBUG_LEVEL must be specified"
+
+# Optional:
+# --autopilot.active
+PARAMS=(
+  "--alias=$ALIAS"
+  "--bitcoin.active"
+  "--lnddir=/lnd"
+  "--tlsextraip=0.0.0.0"
+  "--tlsextradomain=lnd"
+  "--bitcoin.$NETWORK"
+  "--bitcoin.node=$BACKEND"
+  "--$BACKEND.rpccert=/lnd/rpc.cert"
+  "--$BACKEND.rpchost=$RPCHOST"
+  "--$BACKEND.rpcuser=$RPCUSER"
+  "--$BACKEND.rpcpass=$RPCPASS"
+  "--rpclisten=0.0.0.0:10009"
+  "--debuglevel=$DEBUG_LEVEL"
+)
+
+echo "Starting lnd (prod)"
+exec lnd ${PARAMS[*]}
