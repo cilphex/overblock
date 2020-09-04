7fc2c35e6c447ec1096b6e80a6eedec745c45089
Craig Hammell <cilphex@gmail.com>
2020-09-03 21:25:57 -0400

    rtl: reorg startup scripts

---
+++ services/rtl/startup-scripts/add-macaroon-file.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+MACAROON_FILE_PATH="$MACAROON_PATH/admin.macaroon"
+
+# If the admin.macaroon file doesn't exist
+if [[ -f "$MACAROON_FILE_PATH" ]]; then
+  echo "admin.macaroon already exists."
+else
+  echo "admin.macaroon does not exist."
+
+  # And if we have it in the env var as base64
+  if [[ -z "$ADMIN_MACAROON_BASE64" ]]; then
+    echo "base64 admin macaroon is missing in env vars."
+  else
+    echo "base64 admin macaroon is present in env vars."
+
+    # Make the admin macaroon path if it doesn't exist
+    if [[ -d "$MACAROON_PATH" ]]; then
+      echo "macaroon path directory exists: $MACAROON_PATH"
+    else
+      echo "macaroon path directory does not exist. making it: $MACAROON_PATH"
+      mkdir "$MACAROON_PATH"
+    fi
+
+    # Then decode it and write it to the admin.macaroon file
+    echo "$ADMIN_MACAROON_BASE64" > admin.macaroon.base64
+    echo $(base64 -d admin.macaroon.base64) > "$MACAROON_FILE_PATH"
+    rm admin.macaroon.base64
+    echo "created admin.macaroon at $MACAROON_FILE_PATH"
+  fi
+fi
--- services/rtl/startup-scripts/start-rtl.sh
+++ services/rtl/startup-scripts/start-rtl.sh
@@ -35,35 +35,9 @@ assert "$PORT" "PORT must be specified"
 assert "$LN_SERVER_URL" "LN_SERVER_URL must be specified"
 assert "$MACAROON_PATH" "MACAROON_PATH must be specified"
 
-MACAROON_FILE_PATH="$MACAROON_PATH/admin.macaroon"
-echo "okay..."
-
-# If the admin.macaroon file doesn't exist
-if [[ -f "$MACAROON_FILE_PATH" ]]; then
-  echo "admin.macaroon already exists."
-else
-  echo "admin.macaroon does not exist."
-
-  # And if we have it in the env var as base64
-  if [[ -z "$ADMIN_MACAROON_BASE64" ]]; then
-    echo "base64 admin macaroon is missing in env vars."
-  else
-    echo "base64 admin macaroon is present in env vars."
-
-    # Make the admin macaroon path if it doesn't exist
-    if [[ -d "$MACAROON_PATH" ]]; then
-      echo "macaroon path directory exists: $MACAROON_PATH"
-    else
-      echo "macaroon path directory does not exist. making it: $MACAROON_PATH"
-      mkdir "$MACAROON_PATH"
-    fi
-
-    # Then decode it and write it to the admin.macaroon file
-    echo "$ADMIN_MACAROON_BASE64" > admin.macaroon.base64
-    echo $(base64 -d admin.macaroon.base64) > "$MACAROON_FILE_PATH"
-    echo "created admin.macaroon at $MACAROON_FILE_PATH"
-  fi
-fi
+# Create the macaroon file from the base64 version in the env
+# vars, if one does not already exist.
+./add-macaroon-file.sh
 
 # Start rtl
 node rtl
